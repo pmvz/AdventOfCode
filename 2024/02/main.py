796ee7d03d1d0018fee726e49be446f081affba8
Pieter <pmvz_github@outlook.com>
2025-12-13 21:49:19 +0100

    Correct 2024 day 2 part 2

--- 2024/02/main.py
+++ 2024/02/main.py
@@ -1,16 +1,23 @@
 import numpy as np
 
 
-with open("Day 2/input") as file:
+with open("2024/02/input") as file:
     lines = file.readlines()
+
+
+def is_valid_sequence(sequence):
+    """
+    Returns True if the sequence is strictly increasing or 
+    strictly decreasing with absolute differences no greater than 3
+    """
+    diffs = np.diff(sequence)
+    return ((diffs > 0).all() or (diffs < 0).all()) and (abs(diffs) <= 3).all()
+
 
 # Part 1
 safe = 0
 for line in lines:
-    diffs = np.diff(np.array(line.split(), dtype=int))
-    # all decreasing or increasing, and all 1 <= diff <= 3
-    safe += ((diffs > 0).all() or (diffs < 0).all()) and \
-            ((1 <= abs(diffs)) & (abs(diffs) <= 3)).all()
+    safe += is_valid_sequence(np.array(line.split(), dtype=int))
 
 print(safe)
 
@@ -18,37 +25,30 @@ print(safe)
 # Part 2
 safe = 0
 for line in lines:
-    diffs = np.diff(np.array(line.split(), dtype=int))
+    nums = np.array(line.split(), dtype=int)
     
     # Check if no problem dampener is needed
-    is_safe = ((diffs > 0).all() or (diffs < 0).all()) and \
-              ((1 <= abs(diffs)) & (abs(diffs) <= 3)).all()
-    
-    if is_safe:
+    if is_valid_sequence(nums):
         safe += 1
         continue
-
-    # Check errors in the diff range
+    
+    # Find errors
+    diffs = np.diff(nums)
     errors = (abs(diffs) > 3) | (abs(diffs) < 1)
-
-    # Check errors in increase/decrease
-    #np.argmin([(diffs >= 0).sum(), ])
-
-    if (diffs >= 0).sum() >= 1 and (diffs >= 0).sum() < diffs.size-1:
-        errors |= diffs >= 0
-    elif (diffs <= 0).sum() >= 1 and (diffs <= 0).sum() < diffs.size-1:
+    mostly_increasing = (diffs > 0).sum() > (diffs < 0).sum()
+    if mostly_increasing:
         errors |= diffs <= 0
+    else:
+        errors |= diffs >= 0
 
-    if errors.sum() > 1:
-        continue
-    
-    diffs = np.delete(diffs, np.argwhere(errors).flatten())
-    
-    # Check again
-    is_safe = ((diffs > 0).all() or (diffs < 0).all()) and \
-              ((1 <= abs(diffs)) & (abs(diffs) <= 3)).all()
-    
-    if is_safe:
+    # Delete the number before or after the first error and check again
+    indices = np.argwhere(errors).flatten()
+    nums_new_before = np.delete(nums, indices[0])
+    nums_new_after = np.delete(nums, indices[0]+1)
+
+    if is_valid_sequence(nums_new_before) or is_valid_sequence(nums_new_after):
         safe += 1
+        continue
 
-print(safe)  # lower than 611, higher than 514, lower than 551
+
+print(safe)
